b275590bee5ca658891e8fa22ed00dcf078bce66
Zonyitoo <zonyitoo@gmail.com>
2012-10-21 14:18:33 +0800

    README

--- README.md
+++ README.md
@@ -1,15 +1,38 @@
 # gYaH3C
 一个带GUI界面的YaH3C，带有DBus支持和AppIndicator
 
-只在Ubuntu12.10下测试
+## 测试环境
+* Ubuntu12.10 x64
+* 中山大学东校区
+* Python 2.7.3
+
+## 依赖
+* python2.7
+* python-netifaces
+* python-appindicator
+* python-dbus
 
 ## 安装
+首先应先装好依赖关系包
+
+* 可直接从[这里](https://github.com/zonyitoo/gYaH3C/downloads)下载deb包直接安装
+
+```bash
+sudo dpkg -i gyah3c_[VERSION]_all.deb
+```
+
+安装完成后需先启动daemon进程
+```bash
+sudo service gyah3c-daemon start
+```
+
+* 从源码安装
+
 因为使用了DBus的SystemBus，所以要先向系统注册一下下
 ```bash
 sudo cp com.yah3c.EAPDaemon.conf /etc/dbus-1/system.d/
 sudo cp com.yah3c.EAPDaemon.service /usr/share/dbus-1/system-services/
 ```
-
-先运行`daemon.py`，用root权限，然后用普通用户权限运行`main.py`
+然后执行`install.sh`
 ## TODO
 * 用户管理模块
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+SOFTWARE=/usr/share/gYaH3C
+ICON=$SOFTWARE/icon
+DBUS_SYSTEMD=/etc/dbus-1/system.d
+DBUS_SERVICE=/usr/share/dbus-1/system-services
+SYSTEM_PIXMAP=/usr/share/pixmaps
+APP_PATH=/usr/share/applications
+INIT_PATH=/etc/init.d
+
+rm -rf pkg
+mkdir pkg
+mkdir -p $ICON
+
+cp icon/* $ICON
+cp src/*.py $SOFTWARE
+cp src/*.glade $SOFTWARE
+cp src/*.conf $DBUS_SYSTEMD
+cp src/*.service $DBUS_SERVICE
+cp icon/icon.png $SYSTEM_PIXMAP/gYaH3C.png
+cp src/gYaH3C.desktop $APP_PATH
+cp src/gyah3c-daemon $INIT_PATH
+
+sudo ln -s /usr/share/gYaH3C/daemon.py /usr/bin/gyah3c-daemon
+sudo ln -s /usr/share/gYaH3C/main.py /usr/bin/gyah3c
+sudo chmod +x /usr/share/gYaH3C/daemon.py /usr/share/gYaH3C/main.py
